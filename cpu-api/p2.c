3b89712a4f11ccae518add363d0fa52a11e566cb
Baitemirova Aisha <42746632+aisha-alexandra@users.noreply.github.com>
2020-02-13 21:32:17 -0600

    four child processes

--- cpu-api/p2.c
+++ cpu-api/p2.c
@@ -7,20 +7,53 @@ int
 main(int argc, char *argv[])
 {
     printf("HHHHHhello world (pid:%d)\n", (int) getpid());
-    int rc = fork();
+    int rc, rc2,rc3,rc4;
     if (rc < 0) {
         // fork failed; exit
         fprintf(stderr, "fork failed\n");
         exit(1);
-    } else if (rc == 0) {
+    } else{
+        rc=fork();
+        if (rc == 0) {
+            sleep(2);
         // child (new process)
-        printf("hello, I am child (pid:%d)\n", (int) getpid());
-	sleep(1);
-    } else {
-        // parent goes down this path (original process)
-        int wc = wait(NULL);
+        printf("hello, I am a second child (pid:%d)\n", (int) getpid());
+	//sleep(1);
+        }
+        else{
+            rc2=fork();
+            if(rc2==0){
+                sleep(1);
+                printf("hello, I am child (pid:%d)\n", (int) getpid());
+	//sleep(2);
+            }
+            else{
+                rc3=fork();
+                if(rc3==0){
+                    sleep(3);
+                    printf("hello, I am a third child (pid:%d)\n", (int) getpid());
+                }
+                else{
+                    rc4=fork();
+                    if(rc4==0){
+                        sleep(4);
+                        printf("hello, I am a fourth child (pid:%d)\n", (int) getpid());
+                    }
+                        
+            else{
+                sleep(5);
+                int wc = wait(NULL);
         printf("hello, I am parent of %d (wc:%d) (pid:%d)\n",
-	       rc, wc, (int) getpid());
+	       rc,wc, (int) getpid());
     }
     return 0;
+        }
+    }
 }
+}
+}
+
+    
+    
+    
+
